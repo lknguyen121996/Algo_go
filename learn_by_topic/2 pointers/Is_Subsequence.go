9da44da9f4973b0e9a353d8383777da159e18a5d
Long <nguyen_kim_long@DESKTOP-LSPB0AD.localdomain>
2025-09-02 17:01:37 +0700

    feat: add category 2 pointers

---
+++ learn_by_topic/2 pointers/Is_Subsequence.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+/*
+Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+A subsequence of a string is a new string that is formed from the original string by deleting some (can be none)
+of the characters without disturbing the relative positions of the remaining characters.
+(i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+*/
+func IsSubsequence(s, t string) bool {
+	result := true
+	j := 0
+	if len(s) == 0 {
+		return result
+	}
+	for i := 0; i < len(t); i++ {
+		if j > len(s)-1 {
+			break
+		}
+		if s[j] == t[i] {
+			j++
+		}
+	}
+	if j < len(s) {
+		result = false
+	}
+	return result
+}
+
+func main() {
+	type Test struct {
+		s      string
+		t      string
+		result bool
+	}
+	tests := []Test{
+		{"abc", "ahbgdc", true},
+		{"b", "ahbgdc", true},
+		{"axc", "ahbgdc", false},
+	}
+	for _, test := range tests {
+		if result := IsSubsequence(test.s, test.t); result != test.result {
+			fmt.Printf("Input: s: %v, t: %v \n", test.s, test.t)
+			fmt.Printf("Result: %v \n", result)
+			fmt.Printf("Expected Result: %v \n", test.result)
+		}
+	}
+}
